dff639ad9d441448b0ffd3c050bc3eb1a7bd470b
Bogdan Carpusor <bogdan.carpusor@gmail.com>
2019-06-01 13:58:00 +0300

    Initial files

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+  return 'Server Works great great!'
+  
+@app.route('/greet')
+def say_hello():
+  return 'Hello from Server'
+
+if __name__ == '__main__':
+  app.run(debug=True, host='0.0.0.0')
